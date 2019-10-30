cfc3630f606e0f53889e04de600ed17ac7e33124
Vanigami <39018244+Vanigami@users.noreply.github.com>
2019-10-30 19:36:58 -0400

    fixed bug that assigned the same author to each headline

--- lib/GameNews/scraper.rb
+++ lib/GameNews/scraper.rb
@@ -13,12 +13,16 @@ class GameNews::Scraper
       def self.scrape_kotaku
           doc = Nokogiri::HTML(open("https://kotaku.com"))
            
-              kotaku_headline = doc.search('.curation-module__item__wrapper').each_with_index do |title, index|
+          #kotaku_headline = doc.search('.curation-module__item__wrapper').each_with_index do |title,index|
+          kotaku_headline = doc.search('.content-wrapper').each do |title| 
                   headline = GameNews::Headline.new
+                  
                       headline.name = title.css('h6').text.strip
-                      headline_author = doc.css('.content-meta__byline').each do |author|
-                      headline.author = author.css('.item-author').text.strip
-                          end
+                      #headline_author = doc.search('.content-meta__byline').each do |author|
+                      headline.author = title.css('.item-author').text.strip
+                      #headline_url = headline.css('')
+                          #end
+                       
                   #binding.pry
                   end
                   #binding.pry
