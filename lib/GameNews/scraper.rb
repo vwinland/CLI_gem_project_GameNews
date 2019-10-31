b3b082d9f7919f4b0ee7ac7b1cbaea46a9d81075
Vanigami <39018244+Vanigami@users.noreply.github.com>
2019-10-31 10:05:50 -0400

    added url method to headline instance

--- lib/GameNews/scraper.rb
+++ lib/GameNews/scraper.rb
@@ -1,13 +1,14 @@
 class GameNews::Scraper 
     
       def self.scrape_kotaku
-          doc = Nokogiri::HTML(open("https://kotaku.com")
+          doc = Nokogiri::HTML(open("https://kotaku.com"))
 
           kotaku_headline = doc.search('.content-wrapper').each do |title| 
                   headline = GameNews::Headline.new
                   
                       headline.name = title.css('h6').text.strip
                       headline.author = title.css('.item-author').text.strip
+                      headline.url = title.css(".content-meta__headline__wrapper a").attribute('href').value
             end
          end
       end
