50f87db376374b9c8633b5c97a57db545e2a281f
Vanigami <39018244+Vanigami@users.noreply.github.com>
2019-11-03 10:42:02 -0500

    fixed bug in cli.rb that threw an error when input was greater than # of elements in headline array

--- lib/GameNews/cli.rb
+++ lib/GameNews/cli.rb
@@ -10,7 +10,7 @@ class GameNews::CLI
         GameNews::Scraper.scrape_kotaku
         GameNews::Headline.all.each.with_index(1) do |headline, i|
         
-            puts "\n #{i}. #{headline.name} #{headline.feature} #{headline.feature_2} #{headline.feature_3} by #{headline.author} url: #{headline.url} "
+            puts "\n #{i}. #{headline.name} #{headline.feature} #{headline.feature_2} #{headline.feature_3}by #{headline.author} "
         end
     end
 
@@ -21,20 +21,25 @@ class GameNews::CLI
             puts "\n ➔\ Enter the number of the story you'd like more info on or type 'list' to see the list of top headlines again. \n   If you're done, type 'exit'"
             input = gets.strip.downcase
       
-       if input.to_i  < 5 && input.to_i > 0 
+       if input.to_i  <= GameNews::Headline.all.count() && input.to_i > 0 
             the_headline = GameNews::Headline.all[input.to_i-1] 
             puts "\n ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
             puts "#{the_headline.name} #{the_headline.feature} #{the_headline.feature_3} #{the_headline.feature_2} by #{the_headline.author} \n Story Type:  #{the_headline.story_type} "
         elsif input == "list"
             GameNews::Headline.all.clear
             list_stories
+        elsif input == "exit"
+            puts "Check back tomorrow for more videogame news!"
+            exit
         else
             puts "\n Oops, not sure what you want. ☹ \ Type list or exit."
+        
             end
         end
     end
 
     def exit
-     puts "Check back tomorrow for more videogame news!"      
+        #exit the program
     end
+
 end
